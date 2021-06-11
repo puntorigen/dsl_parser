a7421c48768ac7b3973b6c8954988945efe8732a
Pablo Schaffner <pablo@puntorigen.com>
2021-06-11 16:34:15 -0400

    - fixed me->this in addNode method

--- src/index.js
+++ src/index.js
@@ -240,8 +240,9 @@ export default class dsl_parser {
 	 async addNode({ parent_id=this.throwIfMissing('parent_id - addNode'), node=this.throwIfMissing('node - addNode') }={}) {
 		//to do idea: secret icon nodes -> encrypted json with 'secrets' config node within _git.dsl version (cli arg --secret 'pass' y --un_git (crea ver no git con secrets desencriptado))
 		//grab parentID
+		let me = this;
 		let parent = await this.$('node[ID='+parent_id+']').each(async function(i,elem) {
-			let cur = this.$(elem);
+			let cur = me.$(elem);
 			let txml = await this.nodeToXML(node);
 			cur.append(txml);
 		});
